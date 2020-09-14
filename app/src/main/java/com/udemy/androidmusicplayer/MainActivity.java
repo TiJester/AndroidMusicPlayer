1489f349fb022e0da3a7f15a27343a2c17555a16
tijester <tijester@ukr.net>
2020-09-14 15:09:20 +0300

    add func

--- app/src/main/java/com/udemy/androidmusicplayer/MainActivity.java
+++ app/src/main/java/com/udemy/androidmusicplayer/MainActivity.java
@@ -2,13 +2,37 @@ package com.udemy.androidmusicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+    ImageView playPauseIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.thunderstruck); }
+
+    public void previus(View view) {
+    }
+
+    public void next(View view) {
+    }
+
+    public void play(View view) {
+        if (mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+            playPauseIcon.setImageResource(R.drawable.ic_baseline_pause_green_24);
+        } else
+        {
+            mediaPlayer.start();
+       //     playPauseIcon.setImageResource(R.drawable.ic_baseline_play_green_24);
+        }
     }
 }
