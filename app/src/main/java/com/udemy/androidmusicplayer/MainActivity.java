90dec515581eb9469dd421312e912b4b8b84e7da
tijester <tijester@ukr.net>
2020-09-15 05:07:56 +0300

    fix functional

--- app/src/main/java/com/udemy/androidmusicplayer/MainActivity.java
+++ app/src/main/java/com/udemy/androidmusicplayer/MainActivity.java
@@ -6,18 +6,54 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
     ImageView playPauseIcon;
+    SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.thunderstruck); }
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.thunderstruck);
+
+        playPauseIcon = findViewById(R.id.imageViewIcon_play);
+
+        seekBar = findViewById(R.id.seekBar);
+        seekBar.setMax(mediaPlayer.getDuration());
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                seekBar.setProgress(mediaPlayer.getCurrentPosition());
+            }
+        },0,1000);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if (b) {
+                    mediaPlayer.seekTo(i);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
 
     public void previus(View view) {
     }
@@ -28,11 +64,11 @@ public class MainActivity extends AppCompatActivity {
     public void play(View view) {
         if (mediaPlayer.isPlaying()){
             mediaPlayer.pause();
-            playPauseIcon.setImageResource(R.drawable.ic_baseline_pause_green_24);
+            playPauseIcon.setImageResource(R.drawable.ic_baseline_play_green_24);
         } else
         {
             mediaPlayer.start();
-       //     playPauseIcon.setImageResource(R.drawable.ic_baseline_play_green_24);
+            playPauseIcon.setImageResource(R.drawable.ic_baseline_pause_green_24);
         }
     }
 }
